cd7664fd8a66be54573d926872b48a7ade8b171d
Tushar <tusharsuthar6@gmail.com>
2024-01-19 00:52:08 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,81 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+let order = "id";
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = new pg.Client({
+  user: "postgres",
+  database: "booknote",
+  port: 5432,
+  host: "localhost",
+  password: "Tushar@work",
+});
+
+db.connect();
+
+app.get("/", async (req, res) => {
+  const result = await db.query(`SELECT * FROM booknote ORDER BY ${order} ASC`);
+  res.render("index.ejs", { result: result.rows, order: order });
+});
+
+app.get("/add/:id", async (req, res) => {
+  try {
+    const result = await axios.get(
+      `https://openlibrary.org/isbn/${req.params.id}.json`
+    );
+    let data = result.data;
+    res.render("add.ejs", { title: data.title, isbn: req.params.id });
+  } catch (error) {
+    res.status(500).send("Failed to fetch activity. please try again.");
+  }
+});
+
+app.post("/order", function (req, res) {
+  order = req.body.order;
+  res.redirect("/");
+});
+
+app.post("/Submit", async (req, res) => {
+  let { title, description, ISBN, rating } = req.body;
+  if (rating > 5) {
+    rating = 5;
+  } else if (rating < 0) {
+    rating = 0;
+  }
+  await db.query(
+    " INSERT INTO booknote(title, description, isbn, rating ) VALUES  ($1,$2,$3,$4) ",
+    [title, description, ISBN, rating]
+  );
+  res.redirect("/");
+});
+
+app.post("/edit", async (req, res) => {
+  let { title, description, rating, id } = req.body;
+
+  await db.query(
+    "UPDATE booknote SET title = $1, description = $2, rating = $3 WHERE id = $4",
+    [title, description, rating, id]
+  );
+  res.redirect("/");
+});
+
+app.post("/delete", async (req, res) => {
+  req.body.isbn;
+  await db.query("DELETE FROM booknote WHERE isbn = $1", [req.body.isbn]);
+  res.redirect("/");
+});
+
+app.get("/*", (req, res) => {
+  res.redirect("/");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ public/index.js
@@ -0,0 +1,84 @@
+function editBook(id) {
+  document.querySelector(".book" + id).style.display = "none";
+  document.querySelector(".edit" + id).style.display = "flex";
+}
+function closeEdit(id) {
+  document.querySelector(".book" + id).style.display = "flex";
+  document.querySelector(".edit" + id).style.display = "none";
+}
+
+async function fetchapi(event) {
+  document.querySelector(".listSec").innerHTML = "";
+  document.querySelector(".loader").style.display = "block";
+  document.getElementById("dropdown").style.display = "block";
+  let search = document.getElementById("search").value;
+
+
+  try {
+    const response = await fetch(
+      `https://openlibrary.org/search.json?q=${search}&limit=5`
+    );
+
+    const data = await response.json();
+    document.querySelector(".loader").style.display = "none";
+
+    if (data.docs.length > 0) {
+      data.docs.forEach((e) => {
+        let isbn = e.isbn[0];
+        const author = e.author_name.toString();
+
+        const img = document.createElement("img");
+        img.src = `https://covers.openlibrary.org/b/isbn/${isbn}-S.jpg`;
+
+        const title = document.createElement("h3");
+        title.innerText = e.title.substring(0,15)+'..';
+
+        const ISBN = document.createElement("p");
+        ISBN.innerHTML = "<i class='material-icons'>bookmark</i>:" + isbn;
+
+        const writer = document.createElement("p");
+        if (author.length > 15) {
+          writer.innerHTML =
+            "<i class='material-icons'>person</i> " +
+            author.substring(0, 15) +
+            "...";
+        } else {
+          writer.innerHTML = "<i class='material-icons'>person</i> " + author;
+        }
+
+        const btn = document.createElement("button");
+        btn.classList.add("searchInbtn");
+        btn.innerHTML = `<a href="/add/${isbn}"><i class='material-icons'>add</i></a>`;
+
+        const div = document.createElement("div");
+        div.appendChild(title);
+        div.appendChild(ISBN);
+        div.appendChild(writer);
+        div.appendChild(btn)
+
+        const list = document.createElement("li");
+        list.appendChild(img);
+        list.appendChild(div);
+
+        document.querySelector(".listSec").appendChild(list);
+      });
+    } else {
+      const title = document.createElement("H3");
+      title.classList.add("noresult");
+      title.innerHTML =
+        "No result <i class='material-icons'>sentiment_dissatisfied</i>";
+
+      document.querySelector(".listSec").appendChild(title);
+    }
+  } catch (error) {
+    const title = document.createElement("H3");
+    title.classList.add("noresult");
+    title.innerHTML = "API server is down";
+    document.querySelector(".loader").style.display = "none";
+    document.querySelector(".listSec").appendChild(title);
+  }
+}
+
+function closeDropdown() {
+  document.getElementById("dropdown").style.display = "none";
+}
